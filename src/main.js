28e78157fb04c85685617a8cf51da237a7dbe944
ryotaikumi0422 <ryotaikumi0422@gmail.com>
2020-11-16 12:24:55 +0900

    Todoをローカルストレージで保持するように修正

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from "./router";
 
-import todos from "./assets/mockTodo";
 import vuetify from './plugins/vuetify';
+
+import localStorage from "vue-ls";
+const options = {
+  namespace: "vue_todo_ls_", // key prefix
+  name: "ls", // name variable Vue.[ls] or this.[$ls],
+  storage: "local", // storage name session, local, memory
+};
+Vue.use(localStorage, options);
 
 Vue.config.productionTip = false
 
@@ -13,20 +20,28 @@ new Vue({
 
   data: {
     message: "hello",
-    todos,
+    todos: [],
   },
 
   vuetify,
 
   methods: {
+    getTodos() {
+      this.todos = this.$ls.get("todos") || [];
+    },
+    setTodos() {
+      this.$ls.set("todos", this.todos);
+    },
     addTodo(todo) {
       this.todos.push(todo);
+      this.setTodos();
     },
     updateTodo(todo) {
       const id = this.todos.findIndex((item) => item.id == todo.id);
 
       if (id >= 0) {
         this.todos[id].title = todo.title;
+        this.setTodos();
       }
     },
     removeTodo(id) {
@@ -34,7 +49,11 @@ new Vue({
 
       if (index >= 0) {
         this.todos.splice(index, 1);
+        this.setTodos();
       }
     }
+  },
+  mounted() {
+    this.$root.getTodos();
   }
 }).$mount('#app')
